2cf497e934f43ab859b16213ab592206113003d2
lulu-cao <lulu.cao.her@gmail.com>
2024-05-10 14:45:46 -0500

    wrapped the app in electron browser window

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const { join } = require('path');
+const { app, BrowserWindow } = require('electron');
 const { init } = require('@dimensional-innovations/electron-background');
+const path = require('path');
 
-init({
-  appUrl: './index.html',
-  config: {},
-  plugins: []
-});
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+// init({
+//   appUrl: './index.html',
+//   config: {},
+//   plugins: []
+// });
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
