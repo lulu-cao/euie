881f833e08c521cf44f90eeab51ad2143b111265
lulu-cao <lulu.cao.her@gmail.com>
2024-05-14 11:18:36 -0500

    fixed browser window and dev tools using electron-background

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
+// const path = require('path');
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-
-  win.loadFile('index.html')
-}
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// const { init } = require('@dimensional-innovations/electron-background');
-// const path = require('node:path');
-
-// init({
-//   appUrl: `file://${path.resolve('./index.html')}`,
-//   browserWindowOptions: {
+// const createWindow = () => {
+//   const win = new BrowserWindow({
 //     width: 1200,
 //     height: 800,
 //     webPreferences: {
 //       preload: path.join(__dirname, 'preload.js')
-//     },
-//   },
-//   // plugins: [
-//   //   {beforeLoad: () => {
-//   //     browserWindow.on('ready-to-show', () => browserWindow.webContents.openDevTools());
-//   //   }}
-//   // ]
+//     }
+//   })
+
+//   win.loadFile('index.html')
+// }
+// app.whenReady().then(() => {
+//   createWindow()
+
+//   app.on('activate', () => {
+//     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+//   })
 // })
+
+const { init } = require('@dimensional-innovations/electron-background');
+const path = require('node:path');
+
+init({
+  appUrl: `file://${path.resolve('./index.html')}`,
+  browserWindowOptions: {
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    },
+  },
+  plugins: [
+    {
+      beforeLoad(context) {
+        context.browserWindow.on('ready-to-show', () => context.browserWindow.webContents.openDevTools({ mode: 'detach'}));
+      }
+    }
+  ]
+})
 
